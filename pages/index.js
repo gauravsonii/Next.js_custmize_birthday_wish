83053a1f592a4badac5359aefaf96aaed79eda8f
GAURAV SONI <149526482+gauravsonii@users.noreply.github.com>
2025-09-15 10:17:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -82,18 +82,6 @@ export default function Home() {
             </a>
             .
           </p>
-          {/* <p className={styles.desc}>
-            Thanks to all the{" "}
-            <a
-              className={styles.span}
-              href="https://github.com/gouravkhunger/nextjs-birthday-wish/graphs/contributors"
-              target="_blank"
-              rel="noreferrer"
-            >
-              contributors
-            </a>
-            !
-          </p> */}
         </div>
       </main>
     </div>
